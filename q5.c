16f38fad82bf7029da25c5617ae9dbdd9fd26999
yash07004 <113298583+yash07004@users.noreply.github.com>
2022-10-03 20:34:38 +0530

    Add files via upload

---
+++ q5.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    int x= printf("HELLO");
+    printf("\n%d",x);
+    return 0;
+}
---
+++ q6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    printf("\"HELLO,Amit Kumar\"");
+    return 0;
+}
+
+
+
+
+
+
+
